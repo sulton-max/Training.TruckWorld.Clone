2d5972211279cc4a2fdd3efd996a1f46b874eb98
Abdura <abdura52.uz@gmail.com>
2023-09-24 06:19:37 -1200

    Common classes, User and component models created with componentDto

--- src/Training.TruckWorld.Clone.Backend/TruckWorld.Api/Models/Dtos/ComponentDto.cs
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Api/Models/Dtos/ComponentDto.cs
@@ -2,41 +2,40 @@
 using Training.TruckWorld.Backend.Domain.Enums;
 using Action = Training.TruckWorld.Backend.Domain.Enums.Action;
 
-namespace TruckWorld.Api.Models.Dtos
+namespace TruckWorld.Api.Models.Dtos;
+
+public class ComponentDto
 {
-    public class ComponentDto
+    public Guid? Id { get; set; }
+    public Guid? UserId { get; set; }
+    public ComponentCategory Category { get; set; }
+    public string Manufacturer { get; set; }
+    public string Model { get; set; }
+    public string SerialNumber { get; set; }
+    public int Year { get; set; }
+    public int Quantity { get; set; }
+    public double Weight { get; set; }
+    public ComponentCondition Condition { get; set; }
+    public string Description { get; set; }
+    public Action Action { get; set; }
+
+    public ComponentDto(){}
+
+    public ComponentDto(Guid? userId, ComponentCategory category, string manufacturer, string model,
+        string serialNumber, int year, int quantity, double weight, ComponentCondition condition,
+        string description, Action action)
     {
-        public Guid? Id { get; set; }
-        public Guid? UserId { get; set; }
-        ComponentCategory Category { get; set; }
-        public string Manufacturer { get; set; }
-        public string Model { get; set; }
-        public string SerialNumber { get; set; }
-        public int Year { get; set; }
-        public int Quantity { get; set; }
-        public double Weight { get; set; }
-        public ComponentCondition Condition { get; set; }
-        public string Description { get; set; }
-        public Action Action { get; set; }
-
-        public ComponentDto() { }
-
-        public ComponentDto(Guid userId, ComponentCategory category, string manufacturer, string model,
-            string serialNumber, int year, int quantity, double weight, ComponentCondition condition,
-            string description, Action action)
-        {
-            Id = Guid.NewGuid();
-            UserId = userId;
-            Category = category;
-            Manufacturer = manufacturer;
-            Model = model;
-            SerialNumber = serialNumber;
-            Year = year;
-            Quantity = quantity;
-            Weight = weight;
-            Condition = condition;
-            Description = description;
-            Action = action;
-        }
+        Id = Guid.NewGuid();
+        UserId = userId;
+        Category = category;
+        Manufacturer = manufacturer;
+        Model = model;
+        SerialNumber = serialNumber;
+        Year = year;
+        Quantity = quantity;
+        Weight = weight;
+        Condition = condition;
+        Description = description;
+        Action = action;
     }
 }
