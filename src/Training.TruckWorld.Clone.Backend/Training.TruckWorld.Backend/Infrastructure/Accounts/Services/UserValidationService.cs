af02971345edec1fde3bf89ca07cea6325f8687e
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-09-25 22:36:33 +0500

    Updated finished

--- src/Training.TruckWorld.Clone.Backend/Training.TruckWorld.Backend/Infrastructure/Accounts/Services/UserValidationService.cs
+++ src/Training.TruckWorld.Clone.Backend/Training.TruckWorld.Backend/Infrastructure/Accounts/Services/UserValidationService.cs
@@ -6,10 +6,8 @@ namespace Training.TruckWorld.Backend.Infrastructure.Accounts.Services
     public class UserValidationService : IUserValidationService
     {
         public bool IsValidEmailAddress(string emailAddress) => Regex.IsMatch(emailAddress, @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
-        public bool IsValidName(string fullName) => !string.IsNullOrWhiteSpace(fullName);
+        public bool IsValidName(string fullName) => !string.IsNullOrWhiteSpace(fullName) && fullName.Any(c => !Char.IsLetter(c)) && fullName.Any(x => !Char.IsUpper(x));
         public bool IsValidPassword(string password) => Regex.IsMatch(password, @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()-_+=])[A-Za-z\d!@#$%^&*()-_+=]{8,}$");
 
     }
 }
-
-
