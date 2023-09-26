1954315f61e56a32ec1bb969ef0242f63778c873
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-09-26 15:32:16 +0500

    Interface added

---
+++ src/Training.TruckWorld.Clone.Backend/Training.TruckWorld.Backend/Application/Accounts/Services/IUserValidationService.cs
@@ -0,0 +1,9 @@
+﻿namespace Training.TruckWorld.Backend.Application.Accounts.Services
+{
+    public interface IUserValidationService
+    {
+        public bool IsValidName(string name);
+        public bool IsValidPassword(string password);
+        public bool IsValidEmail(string email);
+    }
+}
