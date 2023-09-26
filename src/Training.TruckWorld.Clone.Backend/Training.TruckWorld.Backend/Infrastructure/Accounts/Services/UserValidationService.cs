35955bde7515c71a7f4fa5648e89c2c72583dca6
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-09-26 17:43:33 +0500

    Updated finished

--- src/Training.TruckWorld.Clone.Backend/Training.TruckWorld.Backend/Infrastructure/Accounts/Services/UserValidationService.cs
+++ src/Training.TruckWorld.Clone.Backend/Training.TruckWorld.Backend/Infrastructure/Accounts/Services/UserValidationService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.RegularExpressions;
+using Training.TruckWorld.Backend.Application.Accounts.Services;
 
 namespace Training.TruckWorld.Backend.Infrastructure.Accounts.Services
 {
@@ -6,14 +7,32 @@ namespace Training.TruckWorld.Backend.Infrastructure.Accounts.Services
     {
         public bool IsValidEmail(string email) => Regex.IsMatch(email, @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
 
-        public bool IsValidName(string name)
+        public bool IsValidName(string firstName, string lastName)
         {
-            return !string.IsNullOrEmpty(name)
-                        && char.IsUpper(name[0])
-                        && name.All(letter => char.IsLetter(letter));
+            var capitalizedFirstName = string.Concat(firstName.Substring(0, 1)
+                .ToUpper(), firstName.Substring(1).ToLower());
+
+            var capitalizedLastName = string.Concat(lastName.Substring(0, 1)
+            .ToUpper(), lastName.Substring(1).ToLower());
+
+            if (!string.IsNullOrEmpty(firstName)
+                                && firstName.Equals(capitalizedFirstName)
+                                && firstName.All(letter => char.IsLetter(letter)))
+            if (!string.IsNullOrEmpty(lastName)
+                            && lastName.Equals(capitalizedLastName)
+                            && lastName.All(letter => char.IsLetter(letter)))
+                return true;
+            return false;
+        }
+
+        public bool IsValid(string LastName)
+        {
+            return !string.IsNullOrEmpty(LastName)
+                       && char.IsUpper(LastName[0])
+                       && LastName.All(letter => char.IsLetter(letter));
         }
 
         public bool IsValidPassword(string password) => Regex.IsMatch(password, @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$");
-
+        
     }
 }
