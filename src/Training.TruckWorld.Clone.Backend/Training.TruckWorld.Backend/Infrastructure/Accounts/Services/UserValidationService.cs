13281ec0cd8589dc6c6f9ce61c9353e78f65a531
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-09-26 15:30:04 +0500

    conflict arised a

---
+++ src/Training.TruckWorld.Clone.Backend/Training.TruckWorld.Backend/Infrastructure/Accounts/Services/UserValidationService.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Training.TruckWorld.Backend.Infrastructure.Accounts.Services
+{
+    public class UserValidationService : IUserValidationService
+    {
+        public bool IsValidEmail(string email) => Regex.IsMatch(email, @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
+
+        public bool IsValidName(string name)
+        {
+            return !string.IsNullOrEmpty(name)
+                        && char.IsUpper(name[0])
+                        && name.All(letter => char.IsLetter(letter));
+        }
+
+        public bool IsValidPassword(string password) => Regex.IsMatch(password, @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$");
+
+    }
+}
