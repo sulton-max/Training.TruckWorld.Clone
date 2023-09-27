a2a7c46bf3f3cb8cb272d944fbf00b5e162e15ed
Botirjon <thedavlativich@gmail.com>
2023-09-27 16:59:06 +0500

    ComponentValidationService updated

---
+++ src/Training.TruckWorld.Clone.Backend/Training.TruckWorld.Backend/Application/Components/Interface/IComponentValidationService.cs
@@ -0,0 +1,12 @@
+﻿namespace Training.TruckWorld.Backend.Application.Component.Interface;
+public interface IComponentValidationService
+{
+    bool IsValidManufacturer(string manufacturer);
+    bool IsValidModel(string model);
+    bool IsValidSerialNumber(string serialNumber);
+    bool IsValidYear(int year);
+    bool IdValidQuantity(int quantity);
+    bool IsValidWeight(int weight);
+    bool IsValidDescription(string description);
+
+}
---
+++ src/Training.TruckWorld.Clone.Backend/Training.TruckWorld.Backend/Infrastructure/Components/Services/ComponentValidationService.cs
@@ -0,0 +1,72 @@
+﻿using System.Security.Cryptography;
+
+using Training.TruckWorld.Backend.Application.Component.Interface;
+
+namespace Training.TruckWorld.Backend.Infrastructure.Component.Services;
+
+public class ComponentValidatinService : IComponentValidationService
+{
+
+    public bool IdValidQuantity(int quantity)
+    {
+       if(quantity > 0)
+        {
+            return true;
+        }
+       return false;
+    }
+
+    public bool IsValidDescription(string description)
+    {
+        if (description.Length <= 1000) 
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public bool IsValidManufacturer(string manufacturer)
+    {
+        if(manufacturer.Length < 100 && string.IsNullOrWhiteSpace(manufacturer)) 
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public bool IsValidModel(string model)
+    {
+        if (model.Length < 100 && string.IsNullOrWhiteSpace(model))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public bool IsValidSerialNumber(string serialNumber)
+    {
+        if(serialNumber.Length < 100 && string.IsNullOrWhiteSpace(serialNumber)) 
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public bool IsValidWeight(int weight)
+    {
+        if(weight > 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool IsValidYear(int year)
+    {
+        if(year > 1900 && year < DateTime.Now.Year)
+        {
+            return true;
+        }
+        return false;
+    }
+}
