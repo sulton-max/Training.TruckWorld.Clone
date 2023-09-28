734df76641936cf59ae3ea216549888c0959a27c
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-09-28 12:03:33 +0500

    ComponentFilterSrvice finished you can check this

---
+++ src/Training.TruckWorld.Clone.Backend/Training.TruckWorld.Backend/Application/Components/Services/IComponentFilterService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Training.TruckWorld.Backend.Application.Components.Models.Filters;
+using Training.TruckWorld.Backend.Domain.Entities;
+
+namespace Training.TruckWorld.Backend.Application.Components.Services
+{
+    public interface IComponentFilterService
+    {
+        IQueryable<Component> GetFiltered(ComponentFilterModel filterModel);
+        IQueryable<Component> SearchFiltered(string keyword);
+    }
+}
---
+++ src/Training.TruckWorld.Clone.Backend/Training.TruckWorld.Backend/Infrastructure/Components/Services/ComponentFilterService.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Security;
+using Training.TruckWorld.Backend.Application.Components.Models.Filters;
+using Training.TruckWorld.Backend.Application.Components.Services;
+using Training.TruckWorld.Backend.Application.Configs;
+using Training.TruckWorld.Backend.Domain.Entities;
+using Training.TruckWorld.Backend.Domain.Enums;
+
+
+//###Implement ComponentFilterService
+
+//###Description
+//ComponentFilterService should filter collection of Component model by given ComponentFilterModel
+
+//Requirements
+//-should get filtered collection by given filtermodel
+//-can search components by given keyword
+//-keyword is from component properties:
+//manufacturer or model
+//-service that returns IQueryable
+
+//Deliverables
+//service that filters components
+
+namespace Training.TruckWorld.Backend.Infrastructure.Components.Services
+{
+    public class ComponentFilterService : IComponentFilterService
+    {
+        private List<Component> _components;
+
+        public ComponentFilterService()
+        {
+            _components = new List<Component>();
+        }
+
+        public IQueryable<Component> GetFiltered(ComponentFilterModel filterModel) =>
+            _components.Where(component => component.Manufacturer.ToLower() == 
+            filterModel.Manufacturers.ToString().ToLower()).AsQueryable();
+
+        public IQueryable<Component> SearchFiltered(string keyword)
+             => _components.Where(found => found.Model.ToLower()
+             .Contains(keyword.ToLower())).AsQueryable();
+    }
+}
