1738f5b6706c0b0d6e50278c30ca41c11cfc41ba
Abdura <abdura52.uz@gmail.com>
2023-09-27 13:58:40 -1200

    Validation service for truck ready :)

---
+++ src/Training.TruckWorld.Clone.Backend/Training.TruckWorld.Backend/Application/Trucks/Services/ITruckValidationService.cs
@@ -0,0 +1,38 @@
+﻿using Training.TruckWorld.Backend.Domain.Entities;
+
+namespace Training.TruckWorld.Backend.Application.Trucks.Services;
+
+public interface ITruckValidationService
+{
+    /// <summary>
+    /// string malumotlar uchun e.x: Manufacturer, Model, State, City
+    /// </summary>
+    bool IsValidStuff(string stuff);
+
+    /// <summary>
+    /// Category ni mavjudligi orqali tekshirish
+    /// </summary>
+    bool IsValidCategory(TruckCategory category);
+
+    /// <summary>
+    /// Description ni validligini tekshirish
+    /// </summary>
+    bool IsValidDescription(string description);
+
+    /// <summary>
+    /// Name (first and last name) ni tekshirish
+    /// </summary>
+    bool IsValidName(string name);
+
+    /// <summary>
+    /// Email tekshirish uchun
+    /// </summary>
+    bool IsValidEmailAddress(string email);
+
+    /// <summary>
+    /// phone numberni validligini tekshirish
+    /// </summary>
+    /// <param name="phoneNumber"></param>
+    /// <returns></returns
+    bool IsValidPhoneNumber(string phoneNumber);
+}
---
+++ src/Training.TruckWorld.Clone.Backend/Training.TruckWorld.Backend/Domain/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Training.TruckWorld.Backend.Domain.Extensions;
+
+public static class StringExtensions
+{
+    public static string ToCapitalized(this string str)
+    {
+        return string.Concat(
+            str.Substring(0, 1).ToUpper(),
+            str.Substring(1).ToLower());
+    }
+}
