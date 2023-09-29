ea5af1d32d60ee984bfd8f0bf42208b840a67740
Botirjon <thedavlativich@gmail.com>
2023-09-29 15:12:10 +0500

    create EmailSenderService

---
+++ src/Training.TruckWorld.Clone.Backend/Training.TruckWorld.Backend/Application/Emails/Interfaces/IEmailSenderService.cs
@@ -0,0 +1,8 @@
+﻿using Training.TruckWorld.Backend.Domain.Entities;
+
+namespace Training.TruckWorld.Backend.Application.Emails.Interfaces;
+
+public interface IEmailSenderService
+{
+    ValueTask<Email> SendEmailAsync(Email email);
+}
---
+++ src/Training.TruckWorld.Clone.Backend/Training.TruckWorld.Backend/Infrastructure/Emails/Services/EmailSenderService.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+using Training.TruckWorld.Backend.Application.Emails.Interfaces;
+using Training.TruckWorld.Backend.Domain.Entities;
+
+namespace Training.TruckWorld.Backend.Infrastructure.Emails.Services;
+
+public class EmailSenderService : IEmailSenderService
+{
+    public async ValueTask<bool> SendEmailAsync(Email emailMessage)
+    {
+        var sendEmailMessage = false;
+
+        try
+        {
+            using (var smtp = new SmtpClient("smpt.gmail.com", 587))
+            {
+                smtp.Credentials = new NetworkCredential("username", "password");
+                
+                smtp.EnableSsl = true;
+
+                var mail = new MailMessage(emailMessage.SenderAddress, emailMessage.ReceiverAddress);
+                
+                mail.Subject = emailMessage.Subject;
+            
+                mail.Body = emailMessage.Body;
+              
+                await smtp.SendMailAsync(mail);
+            }
+            emailMessage.IsSent = true;
+           
+            emailMessage.SentTime = DateTimeOffset.UtcNow;
+           
+            sendEmailMessage = true;
+        }
+        catch (Exception ex)
+        {
+            emailMessage.IsSent = false;
+          
+            emailMessage.SentTime = DateTimeOffset.UtcNow;
+         
+            sendEmailMessage = false;
+        }
+        
+        return sendEmailMessage;
+    }
+}
