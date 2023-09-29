d44fd669d1cd1050e9650af6f6017d27821bcc87
Mavlonbek Isroilov <isroilovmavlonbekk@outlook.com>
2023-09-29 18:17:34 +0500

    finished ComponentValidationService

---
+++ src/Training.TruckWorld.Clone.Backend/Training.TruckWorld.Backend/Application/Accounts/Services/IComponentValidationService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training.TruckWorld.Backend.Application.Accounts.Services
+{
+     public interface IComponentValidationService
+    {
+        bool IsValidComponentItem(string item);
+        bool IsValidModel(string model);
+        bool IsValidStockNumber(string stocknumber);
+        bool CheckPhoneNumber(string phonenumber);
+        bool CheckLocation(string country, string city, string street);
+    }
+}
---
+++ src/Training.TruckWorld.Clone.Backend/Training.TruckWorld.Backend/Infrastructure/Accounts/Services/ComponentValidationService.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+using Training.TruckWorld.Backend.Application.Accounts.Services;
+
+namespace Training.TruckWorld.Backend.Infrastructure.Accounts.Services;
+
+public class ComponentValidationService : IComponentValidationService
+{
+    public bool CheckLocation(string country, string city, string street)
+    {
+        if (string.IsNullOrWhiteSpace(country))
+        {
+            return false;
+        }
+        if (string.IsNullOrWhiteSpace(city))
+        {
+            return false;
+        }
+        if (string.IsNullOrWhiteSpace(street))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public bool CheckPhoneNumber(string phoneNumber)
+    {
+        string pattern = @"^(?:\+\d{1,4}\s?)?(?:\d{1,4}[-\s]?)(?:\d{1,4}[-\s]?)(?:\d{1,4})$";
+        return Regex.IsMatch(phoneNumber, pattern);
+    }
+
+    public bool IsValidComponentItem(string item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsValidManiforturer(string maniforturer) => true;
+    public bool IsValidModel(string model) => true;
+    public bool IsValidStockNumber(string stockNumber) => true;
+}
