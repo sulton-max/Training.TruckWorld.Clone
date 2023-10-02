7d771c480bf0dec51c74bc0d7a3a7563a9204663
Habiba67y <sattorovahabiba00@gmail.com>
2023-10-02 15:59:14 +0500

    Created Account and User controllers

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Api/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Training.TruckWorld.Backend.Application.Accounts.Services;
+using Training.TruckWorld.Backend.Domain.Entities;
+using Training.TruckWorld.Backend.Infrastructure.Accounts.Models;
+using Training.TruckWorld.Backend.Infrastructure.Accounts.Services;
+
+namespace TruckWorld.Api.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class AccountController : ControllerBase
+{
+    private readonly IAccountService _accountService;
+    public AccountController(IAccountService accountService)
+    {
+        _accountService = accountService;
+    }
+    [HttpPost]
+    public async ValueTask<IActionResult> Register([FromBody] RegisterDetails regisetrDetails)
+    {
+        var result = await _accountService.Register(regisetrDetails);
+        return result is not null ? Ok(result) : BadRequest();
+    }
+    [HttpGet("{loginDetails: LoginDetails}/Account")]
+    public async ValueTask<IActionResult> Login([FromBody] LoginDetails loginDetails)
+    {
+        var result = await _accountService.Login(loginDetails);
+        return result is not null ? Ok(result) : BadRequest();
+    }
+}
---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Api/Controllers/UserControllers.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Training.TruckWorld.Backend.Application.Accounts.Services;
+using Training.TruckWorld.Backend.Domain.Entities;
+
+namespace TruckWorld.Api.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+
+public class UserControllers: ControllerBase
+{
+    private readonly IUserService _userService;
+    public UserControllers(IUserService userService)
+    {
+        _userService = userService;
+    }
+    [HttpGet("users")]
+    public IActionResult GetAllUsers([FromQuery] int pageToken, [FromQuery] int pageSize, [FromServices] IUserService userService)
+    {
+        var result = userService.Get(user => true).Skip((pageToken - 1) * pageSize).Take(pageSize).ToList();
+        return result.Any() ? Ok(result) : NotFound();
+    }
+
+    [HttpGet("{userId:guid}/user")]
+    public async ValueTask<IActionResult> GetById([FromRoute] Guid userId)
+    {
+        var result = await _userService.GetByIdAsync(userId);
+        return result is not null ? Ok(result) : NotFound();
+    }
+
+    [HttpPost]
+    public async ValueTask<IActionResult> CreateUser([FromBody] User user)
+    {
+        var result = await _userService.CreateAsync(user);
+        return CreatedAtAction(nameof(GetById), new { userId = result.Id }, result);
+    }
+
+    [HttpPut]
+    public async ValueTask<IActionResult> UpdateUser([FromBody] User user)
+    {
+        var result = await _userService.UpdateAsync(user);
+        return NoContent();
+    }
+
+    [HttpDelete("{userId:guid}")]
+    public async ValueTask<IActionResult> DeleteUser([FromRoute] Guid userId)
+    {
+        var result = await _userService.DeleteAsync(userId);
+        return NoContent();
+    }
+}
