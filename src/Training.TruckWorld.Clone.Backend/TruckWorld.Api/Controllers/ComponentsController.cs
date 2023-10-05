006b65e60e1c4e22774fcb1810ea8b670afd0fb3
Abdura <abdura52.uz@gmail.com>
2023-10-05 15:57:04 +0100

    Fixed ,

--- src/Training.TruckWorld.Clone.Backend/TruckWorld.Api/Controllers/ComponentsController.cs
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Api/Controllers/ComponentsController.cs
@@ -13,16 +13,22 @@ public class ComponentsController : ControllerBase
 {
     private readonly IComponentService _componentService;
     private readonly IMapper _mapper;
+
     public ComponentsController(IComponentService componentService, IMapper mapper)
     {
         _componentService = componentService;
         _mapper = mapper;
     }
+
     [HttpGet]
-    public IActionResult GetAllComponents([FromQuery] FilterPagination filterPagination)
+    public IActionResult GetAll([FromQuery] FilterPagination filterPagination)
     {
-        var value = _componentService.Get(user => true).Skip((filterPagination.PageToken - 1) * filterPagination.PageSize).Take(filterPagination.PageSize).ToList();
+        var value = _componentService.Get(user => true)
+            .Skip((filterPagination.PageToken - 1) * filterPagination.PageSize).Take(filterPagination.PageSize)
+            .ToList();
+
         var result = _mapper.Map<List<ComponentDto>>(value);
+
         return result.Any() ? Ok(result) : NotFound();
     }
 
@@ -30,34 +36,46 @@ public class ComponentsController : ControllerBase
     public async ValueTask<IActionResult> GetById([FromRoute] Guid componentId)
     {
         var value = await _componentService.GetByIdAsync(componentId);
+
         var result = _mapper.Map<ComponentDto>(value);
+
         return result is not null ? Ok(result) : NotFound();
     }
 
     [HttpPost]
-    public async ValueTask<IActionResult> CreateComponent([FromBody] ComponentDto componentDto)
+    // [ActionName("GetByIdAsync")]
+    public async ValueTask<IActionResult> Create([FromBody] ComponentDto componentDto)
     {
         var component = _mapper.Map<Component>(componentDto);
+
+        component.UserId = Guid.Parse("0ed10899-a5e4-4424-848d-51875fa59ead");
+
         var value = await _componentService.CreateAsync(component);
+
         var result = _mapper.Map<ComponentDto>(value);
+
         return CreatedAtAction(nameof(GetById), new { componentId = result.Id }, result);
     }
 
     [HttpPut]
-    public async ValueTask<IActionResult> UpdateComponent([FromBody] ComponentDto componentDto)
+    public async ValueTask<IActionResult> Update([FromBody] ComponentDto componentDto)
     {
         var component = _mapper.Map<Component>(componentDto);
+
         var value = await _componentService.UpdateAsync(component);
+
         var result = _mapper.Map<ComponentDto>(value);
+
         return NoContent();
     }
 
     [HttpDelete("{componentId:guid}")]
-    public async ValueTask<IActionResult> DeleteComponent([FromRoute] Guid componentId)
+    public async ValueTask<IActionResult> Delete([FromRoute] Guid componentId)
     {
         var value = await _componentService.DeleteAsync(componentId);
+
         var result = _mapper.Map<ComponentDto>(value);
+
         return NoContent();
     }
 }
-
--- src/Training.TruckWorld.Clone.Backend/TruckWorld.Api/Controllers/TrucksController.cs
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Api/Controllers/TrucksController.cs
@@ -7,22 +7,28 @@ using Training.TruckWorld.Backend.Infrastructure.Filters.Models;
 using TruckWorld.Api.Models.Dtos;
 
 namespace TruckWorld.Api.Controllers;
+
 [ApiController]
 [Route("api/[controller]")]
-public class TrucksController: ControllerBase
+public class TrucksController : ControllerBase
 {
     private readonly ITruckService _truckService;
     private readonly IMapper _mapper;
+
     public TrucksController(ITruckService truckService, IMapper mapper)
     {
         _truckService = truckService;
         _mapper = mapper;
     }
+
     [HttpGet]
-    public IActionResult GetAllTrucks([FromQuery] FilterPagination filterPagination)
+    public IActionResult GetAll([FromQuery] FilterPagination filterPagination)
     {
-        var value = _truckService.Get(user => true).Skip((filterPagination.PageToken - 1) * filterPagination.PageSize).Take(filterPagination.PageSize).ToList();
+        var value = _truckService.Get(user => true).Skip((filterPagination.PageToken - 1) * filterPagination.PageSize)
+            .Take(filterPagination.PageSize).ToList();
+
         var result = _mapper.Map<List<TruckDto>>(value);
+
         return result.Any() ? Ok(result) : NotFound();
     }
 
@@ -30,16 +36,23 @@ public class TrucksController: ControllerBase
     public async ValueTask<IActionResult> GetById([FromRoute] Guid truckId)
     {
         var value = await _truckService.GetByIdAsync(truckId);
+
         var result = _mapper.Map<TruckDto>(value);
+
         return result is not null ? Ok(result) : NotFound();
     }
 
     [HttpPost]
-    public async ValueTask<IActionResult> CreateTruck([FromBody] TruckDto truckDto)
+    public async ValueTask<IActionResult> Create([FromBody] TruckDto truckDto)
     {
         var truck = _mapper.Map<Truck>(truckDto);
+
+        truck.UserId = Guid.Parse("0ed10899-a5e4-4424-848d-51875fa59ead");
+
         var value = await _truckService.CreateAsync(truck);
+
         var result = _mapper.Map<TruckDto>(value);
+
         return CreatedAtAction(nameof(GetById), new { truckId = result.Id }, result);
     }
 
@@ -47,16 +60,17 @@ public class TrucksController: ControllerBase
     public async ValueTask<IActionResult> UpdateTruck([FromBody] TruckDto truckDto)
     {
         var truck = _mapper.Map<Truck>(truckDto);
-        var value = await _truckService.UpdateAsync(truck);
-        var result = _mapper.Map<TruckDto>(value);
+
+        await _truckService.UpdateAsync(truck);
+
         return NoContent();
     }
 
     [HttpDelete("{truckId:guid}")]
     public async ValueTask<IActionResult> DeleteTruck([FromRoute] Guid truckId)
     {
-        var value = await _truckService.DeleteAsync(truckId);
-        var result = _mapper.Map<TruckDto>(value);
+        await _truckService.DeleteAsync(truckId);
+
         return NoContent();
     }
 }
