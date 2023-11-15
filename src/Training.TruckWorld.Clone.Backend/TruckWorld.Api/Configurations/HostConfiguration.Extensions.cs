ec4a1b0bb83e49df977274076b98b1d9b97ba1eb
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-11-15 13:20:24 +0500

    Added HostConfiguration

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Api/Configurations/HostConfiguration.Extensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace TruckWorld.Api.Configurations;
+
+public static partial class HostConfiguration
+{
+    private static WebApplicationBuilder AddExposers(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRouting(options => options.LowercaseUrls = true);
+        builder.Services.AddControllers();
+
+        return builder;
+    }
+
+    private static WebApplicationBuilder AddDevTools(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        return builder;
+    }
+
+    private static WebApplication UseExposers(this WebApplication app)
+    {
+        app.MapControllers();
+
+        return app;
+    }
+
+    private static WebApplication UseDevTools(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        return app;
+    }
+}
---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Api/Configurations/HostConfiguration.cs
@@ -0,0 +1,22 @@
+﻿namespace TruckWorld.Api.Configurations;
+
+public static partial class HostConfiguration
+{
+    public static ValueTask<WebApplicationBuilder> ConfigureAsync(this WebApplicationBuilder builder)
+    {
+        builder
+            .AddExposers()
+            .AddDevTools();
+        
+        return new(builder);
+    }
+
+    public static ValueTask<WebApplication> ConfigureAsync(this WebApplication app)
+    {
+        app
+            .UseExposers()
+            .UseDevTools();
+        
+        return new(app);
+    }
+}
