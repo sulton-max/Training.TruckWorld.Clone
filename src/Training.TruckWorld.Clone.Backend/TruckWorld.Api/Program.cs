a601b1b37300bcec7672576aab1052aeb1ebe7f8
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-11-15 13:19:50 +0500

    Create clean Architecture and added minimal feature

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Api/Program.cs
@@ -0,0 +1,11 @@
+using TruckWorld.Api.Configurations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+await builder.ConfigureAsync();
+
+var app = builder.Build();
+
+await app.ConfigureAsync();
+
+await app.RunAsync();
