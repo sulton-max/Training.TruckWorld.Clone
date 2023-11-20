5655f37e70c9f770e19f126fab4929933a7751f2
Botirjon <thedavlativich@gmail.com>
2023-11-20 18:19:30 +0500

    Add user service

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Identity/Services/IUserService.cs
@@ -0,0 +1,78 @@
+﻿using System.Linq.Expressions;
+using TruckWorld.Domain.Entities;
+
+namespace TruckWorld.Application.Common.Identity.Services;
+
+/// <summary>
+/// Service interface for managing user-related operations.
+/// </summary>
+public interface IUserService
+{
+    /// <summary>
+    /// Retrieves a collection of users based on the specified predicate.
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <param name="asNoTracking"></param>
+    /// <returns></returns>
+    IQueryable<User> Get(Expression<Func<User, bool>>? predicate = default, bool asNoTracking = false);
+
+    /// <summary>
+    /// Retrieves a user by their unique identifier.
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <param name="asNoTracking"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<User?> GetByIdAsync(Guid userId, bool asNoTracking = false, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Retrieves a list of users based on a collection of user IDs.
+    /// </summary>
+    /// <param name="ids"></param>
+    /// <param name="asNoTracking"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<IList<User>> GetByIdsAsync(IEnumerable<Guid> ids, bool asNoTracking = false, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Creates a new user.
+    /// </summary>
+    /// <param name="user"></param>
+    /// <param name="saveChanges"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<User> CreateAsync(User user, bool saveChanges = true, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Updates an existing user.
+    /// </summary>
+    /// <param name="user"></param>
+    /// <param name="saveChanges"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<User> UpdateAsync(User user, bool saveChanges = true, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Deletes a user by their unique identifier.
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <param name="saveChanges"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<User?> DeleteByIdAsync(Guid userId, bool saveChanges = true, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Deletes a user.
+    /// </summary>
+    /// <param name="user"></param>
+    /// <param name="saveChanges"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<User?> DeleteAsync(User user, bool saveChanges = true, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Validates a user based on application-specific criteria.
+    /// </summary>
+    /// <param name="user"></param>
+    void IsValidUser(User user);
+}
---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Infrastructure/Common/Identity/Services/UserService.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+using Microsoft.Extensions.Options;
+
+using TruckWorld.Application.Common.Identity.Services;
+using TruckWorld.Application.Common.Settings;
+using TruckWorld.Domain.Entities;
+using TruckWorld.Persistence.Repositories.Interface;
+
+namespace TruckWorld.Infrastructure.Common.Identity.Services;
+
+public class UserService(IUserRepository userRepository, IOptions<ValidationSettings> validationSettings) : IUserService
+{
+    public ValueTask<User> CreateAsync(
+        User user,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default
+        )
+    {
+        IsValidUser(user);
+
+        return userRepository.CreateAsync(user, saveChanges, cancellationToken);
+    }
+
+    public ValueTask<User?> DeleteByIdAsync(
+        Guid userId,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default
+        )
+    {
+        return userRepository.DeleteByIdAsync(userId, saveChanges, cancellationToken);
+    }
+
+    public IQueryable<User> Get(
+        Expression<Func<User, bool>>? predicate = null,
+        bool asNoTracking = false
+        )
+    {
+        return userRepository.Get(predicate, asNoTracking);
+    }
+
+    public ValueTask<User?> GetByIdAsync(
+        Guid userId,
+        bool asNoTracking = false,
+        CancellationToken cancellationToken = default
+        )
+    {
+        return userRepository.GetByIdAsync(userId, asNoTracking, cancellationToken);
+    }
+
+    public ValueTask<IList<User>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        bool asNoTracking = false,
+        CancellationToken cancellationToken = default
+        )
+    {
+        return userRepository.GetByIdsAsync(ids, asNoTracking, cancellationToken);
+    }
+
+    public ValueTask<User> UpdateAsync(
+        User user,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default
+        )
+    {
+        IsValidUser(user);
+
+        return userRepository.UpdateAsync(user, saveChanges, cancellationToken);
+    }
+
+    public ValueTask<User?> DeleteAsync(
+        User user,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default
+        )
+    {
+        return userRepository.DeleteAsync(user, saveChanges, cancellationToken);
+    }
+
+    public void IsValidUser(User user)
+    {
+        if (!Regex.IsMatch(user.EmailAddress, validationSettings.Value.EmailRegexPattern))
+            throw new InvalidOperationException("Invalid email address");
+
+        if (!Regex.IsMatch(user.Password, validationSettings.Value.PasswordRegexPattern))
+            throw new InvalidOperationException("Invalid password");
+    }
+
+}
