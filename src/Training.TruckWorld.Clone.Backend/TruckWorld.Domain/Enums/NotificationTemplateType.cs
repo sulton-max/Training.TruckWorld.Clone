80f3fc594b3079ab75731cc99d94fac8e98cbf75
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-11-20 16:03:30 +0500

    Created NotificationType and NotificationTemplateType

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Domain/Enums/NotificationTemplateType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruckWorld.Domain.Enums
+{
+    public enum NotificationTemplateType
+    {
+        Welcomenotification = 0,
+        EmailAddressVerificationNotification = 1,
+        PhoneNumberVerificationnotification = 2,
+        ReferralNotification = 3
+    }
+}
---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Domain/Enums/NotificationType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruckWorld.Domain.Enums
+{
+    public enum NotificationType
+    {
+        Email = 0,
+        Sms = 1
+    }
+}
