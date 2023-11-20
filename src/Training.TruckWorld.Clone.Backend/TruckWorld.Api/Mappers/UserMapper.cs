657ea13f9fe379d08e569bc3999282bce9360b78
Botirjon <thedavlativich@gmail.com>
2023-11-20 18:20:29 +0500

    Add user dto and mappers

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Api/Mappers/UserMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+
+using TruckWorld.Api.Models.DTOs;
+using TruckWorld.Domain.Entities;
+
+namespace TruckWorld.Api.Mappers;
+
+/// <summary>
+/// AutoMapper profile for mapping between the User and UserDto classes.
+/// </summary>
+public class UserMapper : Profile
+{
+    /// <summary>
+    /// Initializes a new instance of the UserProfile
+    /// </summary>
+    public UserMapper()
+    {
+        CreateMap<User, UserDto>().ReverseMap();
+    }
+}
---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Api/Models/DTOs/UserDto.cs
@@ -0,0 +1,27 @@
+﻿namespace TruckWorld.Api.Models.DTOs;
+
+/// <summary>
+/// Data transfer object (DTO) representing user information.
+/// </summary>
+public class UserDto
+{
+    /// <summary>
+    /// Gets or sets the first name of the user
+    /// </summary>
+    public string FirstName { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the last name of the user 
+    /// </summary>
+    public string LastName { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the email address of the user
+    /// </summary>
+    public string EmailAddress { get; set; } = default!;
+
+    /// <summary>
+    ///Gets or sets the password of the user 
+    /// </summary>
+    public string Password { get; set; } = default!;
+}
