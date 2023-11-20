9c9881747eafd737c4dd0382cd3a696d2658be4e
Botirjon <thedavlativich@gmail.com>
2023-11-20 18:15:43 +0500

    Add user to dbContext and  add user configuration

--- src/Training.TruckWorld.Clone.Backend/TruckWorld.Persistence/DataContext/NotificationsDbContext.cs
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Persistence/DataContext/NotificationsDbContext.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
+using TruckWorld.Domain.Entities;
+
 namespace TruckWorld.Persistence.DataContext;
 
+/// <summary>
+/// Represents the database context for managing notifications-related entities
+/// </summary>
 public class NotificationsDbContext : DbContext
 {
+    /// <summary>
+    /// Gets or Sets the DbSets for managing User entities
+    /// </summary>
+    public DbSet<User> Users => Set<User>();
+
     public NotificationsDbContext(DbContextOptions<NotificationsDbContext> dbContextOptions) : base(dbContextOptions)
     {
     }
