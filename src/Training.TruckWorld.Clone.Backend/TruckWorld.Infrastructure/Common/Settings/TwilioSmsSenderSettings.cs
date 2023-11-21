f7311c7a46a2fe47129d84fa882b12dadf8514e6
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-11-21 22:46:35 +0500

    created notification sender service

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Infrastructure/Common/Settings/TwilioSmsSenderSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace TruckWorld.Infrastructure.Common.Settings;
+
+public class TwilioSmsSenderSettings
+{
+    public string AccountSid { get; set; } = default!;
+
+    public string AuthToken { get; set; } = default!;
+
+    public string SenderPhoneNumber { get; set; } = default!;
+}
