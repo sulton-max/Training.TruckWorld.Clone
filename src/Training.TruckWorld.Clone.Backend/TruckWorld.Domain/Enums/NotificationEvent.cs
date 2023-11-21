16cff66905aa9a2963c43dfdc6fac6fdf8d28edd
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-11-21 21:39:44 +0500

    created "ExceptionExtensions" extension. Added commit to FuncResult, NotificaitonEvent, NotificationTypeand ExcetptionExtentions.

--- src/Training.TruckWorld.Clone.Backend/TruckWorld.Domain/Enums/NotificationEvent.cs
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Domain/Enums/NotificationEvent.cs
@@ -1,8 +1,22 @@
 ﻿namespace TruckWorld.Domain.Enums;
 
+/// <summary>
+/// Represents a notification event
+/// </summary>
 public enum NotificationEvent
 {
+    /// <summary>
+    /// status in rendering the notification
+    /// </summary>
     OnRedering,
+
+    /// <summary>
+    /// status in sending the notificaiton
+    /// </summary>
     OnSending,
+
+    /// <summary>
+    /// status in saving the notificaiton
+    /// </summary>
     OnSaving,
 }
