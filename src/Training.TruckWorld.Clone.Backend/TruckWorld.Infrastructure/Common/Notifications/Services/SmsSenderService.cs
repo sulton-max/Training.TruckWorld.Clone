7c40001f487ea5ce610431a44f3a78db4febb370
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-11-22 00:17:07 +0500

    Created SmsSenderService service

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Infrastructure/Common/Notifications/Services/SmsSenderService.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using TruckWorld.Application.Common.Brokers;
+using TruckWorld.Application.Common.Models;
+using TruckWorld.Application.Common.Services;
+using TruckWorld.Domain.Enums;
+using TruckWorld.Domain.Extensions;
+
+namespace TruckWorld.Infrastructure.Common.Notifications.Services;
+
+public class SmsSenderService : ISmsSenderService
+{
+    private readonly IEnumerable<ISmsSenderBroker> _smsSenderBroker;
+    private readonly IValidator<SmsMessage> _smsSenderValidator;
+
+    public SmsSenderService(IEnumerable<ISmsSenderBroker> smsSenderBroker, IValidator<SmsMessage> smsSenderValidator)
+    {
+        _smsSenderBroker = smsSenderBroker;
+        _smsSenderValidator = smsSenderValidator;
+    }
+
+    public async ValueTask<bool> SendAsync(SmsMessage smsMessage, CancellationToken cancellationToken)
+    {
+        var validationResult = _smsSenderValidator.Validate(smsMessage,
+            options => options.IncludeRuleSets(NotificationEvent.OnRedering.ToString()));
+
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        foreach (var broker in _smsSenderBroker)
+        {
+            var sendNotification = () => broker.SendAsync(smsMessage, cancellationToken);
+
+            var result = await sendNotification.GetValueAsync();
+
+            smsMessage.IsSuccessfull = result.IsSuccess;
+            smsMessage.ErrorMessage = result.Exception?.Message;
+
+            return smsMessage.IsSuccessfull;
+        }
+
+        return false;
+    }
+}
