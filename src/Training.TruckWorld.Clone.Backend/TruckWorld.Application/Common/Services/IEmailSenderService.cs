5dd2190c98a207f1101767893a73fc71982e1a4b
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-11-21 22:11:57 +0500

    Created IEmailSenderService and ISmsSenderService services

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Services/IEmailSenderService.cs
@@ -0,0 +1,8 @@
+﻿using TruckWorld.Application.Common.Models;
+
+namespace TruckWorld.Application.Common.Services;
+
+public interface IEmailSenderService
+{
+    ValueTask<bool> SendAsync(EmailMessage emailMessage, CancellationToken cancellationToken);
+}
