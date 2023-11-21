f114a1cd84000df9bc502da05bf2a7822781384e
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-11-21 21:43:42 +0500

    Created EmailTemplate and SmsTemplate entities

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Domain/Entities/EmailTemplate.cs
@@ -0,0 +1,13 @@
+﻿using TruckWorld.Domain.Common.Entities;
+using TruckWorld.Domain.Enums;
+
+namespace TruckWorld.Domain.Entities;
+
+public class EmailTemplate : IEntity
+{
+    public EmailTemplate() => Type = NotificationType.Email;
+
+    public Guid Id { get; set; }
+
+    public NotificationType Type { get; set; }
+}
