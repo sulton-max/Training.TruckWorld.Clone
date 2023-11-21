5fcfa476272624e4da8332122f4936763fb91346
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-11-21 22:41:44 +0500

    cerated NotificationMessageMapper mapper. And created EmailMessageValidator and SmsMessageValidator validators

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Infrastructure/Common/Validators/SmsMessageValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using TruckWorld.Application.Common.Models;
+using TruckWorld.Domain.Enums;
+
+namespace TruckWorld.Infrastructure.Common.Validators;
+
+public class SmsMessageValidator : AbstractValidator<SmsMessage>
+{
+    public SmsMessageValidator()
+    {
+        RuleSet(NotificationEvent.OnRedering.ToString(),
+            () =>
+            {
+                RuleFor(message => message.Template).NotNull();
+                RuleFor(message => message.Variables).NotNull();
+            });
+
+        RuleSet(NotificationEvent.OnSending.ToString(),
+            () =>
+            {
+                RuleFor(message => message.SenderPhoneNumber).NotNull().NotEmpty();
+                RuleFor(message => message.RecieverPhoneNumber).NotNull().NotEmpty();
+                RuleFor(message => message.Message).NotNull().NotEmpty();
+            });
+    }
+}
