8a509443488c771d24cef0c4df34f1d09a7c3ad9
Botirjon <thedavlativich@gmail.com>
2023-11-22 12:58:13 +0500

    Add verification properties to user and validation settings

--- src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Settings/ValidationSettings.cs
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Settings/ValidationSettings.cs
@@ -14,4 +14,14 @@ public class ValidationSettings
     /// Gets or sets the regular expression pattern for validating passwords.
     /// </summary>
     public string PasswordRegexPattern { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the duration, in seconds, for which a verification code remains valid.
+    /// </summary>
+    public int VerificationCodeExpiryTimeInSeconds { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the regular expression pattern used for URL validation.
+    /// </summary>
+    public string UrlRegexPattern { get; set; } = default!;
 }
--- src/Training.TruckWorld.Clone.Backend/TruckWorld.Domain/Entities/User.cs
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Domain/Entities/User.cs
@@ -26,4 +26,19 @@ public class User : SoftDeletedEntity
     ///Gets or sets the password of the user 
     /// </summary>
     public string PasswordHash { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the DateTimeOffset representing the expiry time.
+    /// </summary>
+    public DateTime ExpiryTime { get; set; }
+
+    /// <summary>
+    /// Gets or sets a boolean indicating the activation status.
+    /// </summary>
+    public bool IsActive { get; set; }
+
+    /// <summary>
+    /// Gets or sets the verification link string.
+    /// </summary>
+    public string VerificationLink { get; set; } = string.Empty;
 }
