f6ab928efa28d0487b09d65559685f61984ec3c0
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-11-23 12:51:57 +0500

    created filterPagination for all templates

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Models/Querying/FilterPagination.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruckWorld.Application.Common.Models.Querying;
+
+/// <summary>
+/// created Pagination for templates
+/// </summary>
+public class FilterPagination
+{
+    public uint PageSize { get; set; } = 10;
+
+    public uint PageToken { get; set; } = 1;
+}
