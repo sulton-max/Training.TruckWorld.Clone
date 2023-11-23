c99df02dae1eec79ade68d21e592832d633e7af4
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-11-23 20:08:40 +0500

    add doc to EmailMessage entity

--- src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Models/EmailMessage.cs
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Models/EmailMessage.cs
@@ -2,15 +2,33 @@
 
 namespace TruckWorld.Application.Common.Models;
 
+/// <summary>
+/// Represent a email message
+/// </summary>
 public class EmailMessage : NotificationMessage
 {
+    /// <summary>
+    /// Gets or sets sender the email address
+    /// </summary>
     public string SenderEmailAddress { get; set; } = default!;
 
+    /// <summary>
+    /// Gets or sets the reciever email address
+    /// </summary>
     public string RecieverEmailAddress { get; set; } = default!;
 
+    /// <summary>
+    /// Gets or sets EmailTemplate entities
+    /// </summary>
     public EmailTemplate Template { get; set; } = default!;
 
+    /// <summary>
+    /// Gets or sets subject of the messages
+    /// </summary>
     public string Subject { get; set; } = default!;
 
+    /// <summary>
+    /// Gets or sets body of the message
+    /// </summary>
     public string Body { get; set; } = default!;
 }
