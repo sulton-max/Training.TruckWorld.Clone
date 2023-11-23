de85ae63c11112d8b62b1f925d5562a96e00fb2e
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-11-23 13:15:03 +0500

    created midgrations for created entities

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Persistence/Migrations/20231123064322_AddMigration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TruckWorld.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NotificationTemplates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    TemplateType = table.Column<int>(type: "integer", nullable: false),
+                    Content = table.Column<string>(type: "character varying(129536)", maxLength: 129536, nullable: false),
+                    Subject = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotificationTemplates", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NotificationTemplates_Type_TemplateType",
+                table: "NotificationTemplates",
+                columns: new[] { "Type", "TemplateType" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NotificationTemplates");
+        }
+    }
+}
