93ccd819a845a2cd091a99443246f85dab3e6bd7
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-11-23 13:05:58 +0500

    created LinqExtensions to do  filterPaginations

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Querying/Extensions/LinqExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TruckWorld.Application.Common.Models.Querying;
+
+namespace TruckWorld.Application.Common.Querying.Extensions;
+
+/// <summary>
+/// created two extension methods to do Paginations
+/// </summary>
+public static class LinqExtensions
+{
+    public static IQueryable<TSource> ApplyPagination<TSource>(this IQueryable<TSource> source, FilterPagination paginationOptions)
+    {
+        return source.Skip((int)((paginationOptions.PageToken - 1) * paginationOptions.PageSize)).Take((int) paginationOptions.PageSize);
+    }
+
+    public static IEnumerable<TSource> ApplyPagination<TSource>(this IEnumerable<TSource> source, FilterPagination paginationOptions)
+    {
+        return source.Skip((int)((paginationOptions.PageToken - 1) * paginationOptions.PageSize)).Take((int)paginationOptions.PageSize);
+    }
+}
