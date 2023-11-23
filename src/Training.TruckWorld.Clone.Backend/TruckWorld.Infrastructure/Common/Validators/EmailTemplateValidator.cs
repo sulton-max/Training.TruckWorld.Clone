3092f2b637330cbd490a04b9a23325d12581354a
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-11-23 13:12:32 +0500

    creted two validators SmsTemplateValidator and EmailTemplateValidator

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Infrastructure/Common/Validators/EmailTemplateValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TruckWorld.Domain.Entities;
+using TruckWorld.Domain.Enums;
+
+namespace TruckWorld.Infrastructure.Common.Validators
+{
+    public class EmailTemplateValidator : AbstractValidator<EmailTemplate>
+    {
+        public EmailTemplateValidator()
+        {
+            RuleFor(template => template.Content)
+                .NotEmpty()
+                .MinimumLength(10)
+                .MaximumLength(256);
+
+            RuleFor(template => template.Type)
+                .Equal(NotificationType.Email);
+        }
+    }
+}
---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Infrastructure/Common/Validators/SmsTemplateValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TruckWorld.Domain.Entities;
+using TruckWorld.Domain.Enums;
+
+namespace TruckWorld.Infrastructure.Common.Validators
+{
+    public class SmsTemplateValidator : AbstractValidator<SmsTemplate>
+    {
+        public SmsTemplateValidator()
+        {
+            RuleFor(template => template.Content)
+                .NotEmpty()
+                .WithMessage("Sms template content is required")
+                .MinimumLength(10)
+                .WithMessage("Sms template content must be at least 10 characters long")
+                .MaximumLength(256)
+                .WithMessage("Sms template content must be at most 256 characters long");
+
+            RuleFor(template => template.Type)
+                .Equal(NotificationType.Sms)
+                .WithMessage("Sms template notification type must be Sms");
+        }
+    }
+}
