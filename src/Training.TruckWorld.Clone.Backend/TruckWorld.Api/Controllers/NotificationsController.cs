718b560c0f50d785caa2cc79f40fed8790cc281b
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-11-23 13:22:30 +0500

    added NotificationsContoller for two entities SmsTemplate and EmailTemplate

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Api/Controllers/NotificationsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TruckWorld.Application.Common.Models.Querying;
+using TruckWorld.Application.Common.Notifications.Services;
+using TruckWorld.Domain.Entities;
+
+namespace TruckWorld.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NotificationsController : ControllerBase
+{   
+    private readonly IEmailTemplateService _emailTemplateService;
+    private readonly ISmsTemplateService _smsTemplateService;
+
+    public NotificationsController(IEmailTemplateService emailTemplateService, ISmsTemplateService smsTemplateService)
+    {
+        _emailTemplateService = emailTemplateService;
+        _smsTemplateService = smsTemplateService;
+    }
+
+    [HttpGet("sms")]
+    public async ValueTask<IActionResult> GetSmsTemplates([FromQuery] FilterPagination pagination)
+    {
+        var result = await _smsTemplateService.GetByFilterAsync(pagination);
+        return Ok(result);
+    }
+
+    [HttpGet("email")]
+    public async ValueTask<IActionResult> GetEmailTemplates([FromQuery] FilterPagination filterPagination)
+    {
+        var result = await _emailTemplateService.GetByFilterAsync(filterPagination);
+        return Ok(result);
+    }
+}
