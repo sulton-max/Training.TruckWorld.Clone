7e52a64b2b38f821b30abf7c7831dd020320b6f6
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-11-23 20:16:46 +0500

    add doc to NotificationMessage entity

--- src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Models/NotificationMessage.cs
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Models/NotificationMessage.cs
@@ -1,14 +1,32 @@
 ﻿namespace TruckWorld.Application.Common.Models;
 
+/// <summary>
+/// Represent a notification message
+/// </summary>
 public class NotificationMessage
 {
+    /// <summary>
+    /// Gets or sets sender ids
+    /// </summary>
     public Guid SenderId { get; set; }
 
+    /// <summary>
+    /// Gets or sets reciever ids
+    /// </summary>
     public Guid RecieverId { get; set; }
 
+    /// <summary>
+    /// Gets or sets variables
+    /// </summary>
     public Dictionary<string, string> Variables { get; set; }
 
+    /// <summary>
+    /// Gets or sets that notification is sent or not 
+    /// </summary>
     public bool IsSuccessfull { get; set; }
 
+    /// <summary>
+    /// Gets or sets error message while was sending message
+    /// </summary>
     public string? ErrorMessage { get; set; }
 }
