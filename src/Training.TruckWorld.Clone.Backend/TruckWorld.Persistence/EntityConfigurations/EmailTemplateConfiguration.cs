5b949f47180fc2d77c78ca9ac648b71743d798e5
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-11-23 13:16:55 +0500

    created three configurations for EmailTemplate, NotificationTemplate and SmsTemplate

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Persistence/EntityConfigurations/EmailTemplateConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TruckWorld.Domain.Entities;
+
+namespace TruckWorld.Persistence.EntityConfigurations
+{
+    public class EmailTemplateConfiguration : IEntityTypeConfiguration<EmailTemplate>
+    {
+        public void Configure(EntityTypeBuilder<EmailTemplate> builder)
+        {
+            builder.Property(template => template.Subject).IsRequired().HasMaxLength(256);
+        }
+    }
+}
---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Persistence/EntityConfigurations/SmsTemplateConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TruckWorld.Domain.Entities;
+
+namespace TruckWorld.Persistence.EntityConfigurations
+{
+    public class SmsTemplateConfiguration : IEntityTypeConfiguration<SmsTemplate>
+    {
+        public void Configure(EntityTypeBuilder<SmsTemplate> builder)
+        {
+
+        }
+    }
+}
