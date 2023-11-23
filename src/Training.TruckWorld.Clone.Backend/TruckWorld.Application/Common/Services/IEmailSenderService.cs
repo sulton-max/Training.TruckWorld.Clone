0a67cf2b5c06e20c182028546d0b6198af105e76
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-11-23 20:27:20 +0500

    add doc to Notification sender service

--- src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Services/IEmailSenderService.cs
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Services/IEmailSenderService.cs
@@ -2,7 +2,16 @@
 
 namespace TruckWorld.Application.Common.Services;
 
+/// <summary>
+/// Represent sending email messages
+/// </summary>
 public interface IEmailSenderService
 {
+    /// <summary>
+    /// Function that sends email messages
+    /// </summary>
+    /// <param name="emailMessage"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     ValueTask<bool> SendAsync(EmailMessage emailMessage, CancellationToken cancellationToken = default);
 }
--- src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Services/ISmsSenderService.cs
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Services/ISmsSenderService.cs
@@ -1,8 +1,16 @@
 ﻿using TruckWorld.Application.Common.Models;
 
 namespace TruckWorld.Application.Common.Services;
-
+/// <summary>
+/// Represent sending sms messages
+/// </summary>
 public interface ISmsSenderService
 {
+    /// <summary>
+    /// Function that sends sms messages
+    /// </summary>
+    /// <param name="smsMessage"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     ValueTask<bool> SendAsync(SmsMessage smsMessage, CancellationToken cancellationToken = default);
 }
