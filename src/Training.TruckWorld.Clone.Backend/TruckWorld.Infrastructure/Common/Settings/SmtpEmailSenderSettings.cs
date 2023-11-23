73f5d18b3dda7d251671f7e669bcdcbb70070341
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-11-23 20:38:29 +0500

    add doc to notification sender setting

--- src/Training.TruckWorld.Clone.Backend/TruckWorld.Infrastructure/Common/Settings/SmtpEmailSenderSettings.cs
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Infrastructure/Common/Settings/SmtpEmailSenderSettings.cs
@@ -1,12 +1,27 @@
 ﻿namespace TruckWorld.Infrastructure.Common.Settings;
 
+/// <summary>
+/// Represent a sending setting of email
+/// </summary>
 public class SmtpEmailSenderSettings
 {
+    /// <summary>
+    /// Gets or sets host
+    /// </summary>
     public string Host { get; set; } = default!;
 
+    /// <summary>
+    /// Gets or sets port
+    /// </summary>
     public int Port { get; set; }
 
+    /// <summary>
+    /// Gets or sets a sender's address
+    /// </summary>
     public string CredentialAddress { get; set; } = default!;
 
+    /// <summary>
+    /// Gets or sets a sender's email password
+    /// </summary>
     public string Password { get; set; } = default!;
 }
--- src/Training.TruckWorld.Clone.Backend/TruckWorld.Infrastructure/Common/Settings/TwilioSmsSenderSettings.cs
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Infrastructure/Common/Settings/TwilioSmsSenderSettings.cs
@@ -1,10 +1,22 @@
 ﻿namespace TruckWorld.Infrastructure.Common.Settings;
 
+/// <summary>
+/// Represent a sending setting of sms
+/// </summary>
 public class TwilioSmsSenderSettings
 {
+    /// <summary>
+    /// Gets or sets AccountSid
+    /// </summary>
     public string AccountSid { get; set; } = default!;
 
+    /// <summary>
+    /// Gets or sets Authrization Token
+    /// </summary>
     public string AuthToken { get; set; } = default!;
 
+    /// <summary>
+    /// Gets or sets sender's phone numbers
+    /// </summary>
     public string SenderPhoneNumber { get; set; } = default!;
 }
