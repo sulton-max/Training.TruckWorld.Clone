831d11490c107ca0fc825aba38291a77d60f9592
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-11-23 13:23:23 +0500

    done allTasks and working all the Services and methods

--- src/Training.TruckWorld.Clone.Backend/TruckWorld.Api/Program.cs
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Api/Program.cs
@@ -1,10 +1,17 @@
 using TruckWorld.Api.Configurations;
+using TruckWorld.Application.Common.Notifications.Services;
+using TruckWorld.Infrastructure.Common.Notifications.Services;
+using TruckWorld.Persistence.Repositories;
+using TruckWorld.Persistence.Repositories.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 await builder.ConfigureAsync();
+
 
 var app = builder.Build();
 
 await app.ConfigureAsync();
+
 await app.RunAsync();
