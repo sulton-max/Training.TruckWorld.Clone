8a0f63fa6f24dde41d1a35295336a792abdb79b0
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-11-23 20:23:37 +0500

    add doc to Notificaiton brokers

--- src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Brokers/ISmsSenderBroker.cs
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Brokers/ISmsSenderBroker.cs
@@ -2,7 +2,16 @@
 
 namespace TruckWorld.Application.Common.Brokers;
 
+/// <summary>
+/// Represent sending sms messages
+/// </summary>
 public interface ISmsSenderBroker
 {
+    /// <summary>
+    /// Function that sends sms messages
+    /// </summary>
+    /// <param name="smsMessage"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     ValueTask<bool> SendAsync(SmsMessage smsMessage, CancellationToken cancellationToken = default);
 }
