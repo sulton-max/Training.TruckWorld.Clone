1510ca1782fecb32bdde2b0e496a8d9993da864c
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-11-24 11:49:33 +0500

    created SmsMessage entity

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Notificaitons/Models/SmsMessage.cs
@@ -0,0 +1,29 @@
+﻿using TruckWorld.Domain.Entities;
+
+namespace TruckWorld.Application.Common.Notificaitons.Models;
+
+/// <summary>
+/// Represents a sms message
+/// </summary>
+public class SmsMessage : NotificationMessage
+{
+    /// <summary>
+    /// Gets or sets senders' phone numbers
+    /// </summary>
+    public string SenderPhoneNumber { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets recievers' phone numbers
+    /// </summary>
+    public string RecieverPhoneNumber { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets SmsTemplate entities
+    /// </summary>
+    public SmsTemplate Template { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the sms messages
+    /// </summary>
+    public string Message { get; set; } = default!;
+}
