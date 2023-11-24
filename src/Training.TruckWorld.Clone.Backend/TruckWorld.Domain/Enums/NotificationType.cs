d53ec2ba02326f98ef1a4eafb8ef2a6712a01364
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-11-24 11:33:36 +0500

    added NotificationType enum

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Domain/Enums/NotificationType.cs
@@ -0,0 +1,17 @@
+﻿namespace TruckWorld.Domain.Enums;
+
+/// <summary>
+/// Represents a notification type
+/// </summary>
+public enum NotificationType
+{
+    /// <summary>
+    /// indicates that the notification type is email
+    /// </summary>
+    Email = 0,
+
+    /// <summary>
+    /// indicates that the notification type is sms
+    /// </summary>
+    Sms = 1,
+}
