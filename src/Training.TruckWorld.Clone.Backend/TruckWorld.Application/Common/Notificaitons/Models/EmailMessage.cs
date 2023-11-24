f3aa8604579ab044d4325a3cc2e129f84c78e148
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-11-24 11:48:28 +0500

    created EmailMessage entity

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Notificaitons/Models/EmailMessage.cs
@@ -0,0 +1,34 @@
+﻿using TruckWorld.Domain.Entities;
+
+namespace TruckWorld.Application.Common.Notificaitons.Models;
+
+/// <summary>
+/// Represents a email message
+/// </summary>
+public class EmailMessage : NotificationMessage
+{
+    /// <summary>
+    /// Gets or sets senders' the email address
+    /// </summary>
+    public string SenderEmailAddress { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the recievers' email address
+    /// </summary>
+    public string RecieverEmailAddress { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets EmailTemplate entities
+    /// </summary>
+    public EmailTemplate Template { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets subject of the messages
+    /// </summary>
+    public string Subject { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets body of the message
+    /// </summary>
+    public string Body { get; set; } = default!;
+}
