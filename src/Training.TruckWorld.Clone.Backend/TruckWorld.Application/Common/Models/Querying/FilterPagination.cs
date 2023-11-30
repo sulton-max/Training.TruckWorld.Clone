95f3b1e70277fa5749462adce81a50a869dfcb78
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-11-30 12:04:23 +0500

    added filterPagination and linqExtensions to Architucture

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Models/Querying/FilterPagination.cs
@@ -0,0 +1,8 @@
+﻿namespace TruckWorld.Application.Common.Models.Querying;
+
+public class FilterPagination
+{
+    public uint PageSize { get; set; } = 10;
+
+    public uint PageToken { get; set; } = 1;
+}
