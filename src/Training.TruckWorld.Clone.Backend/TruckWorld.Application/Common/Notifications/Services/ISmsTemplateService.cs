a90c9bf7069a760d8b5e27243a746741d55bf38a
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-11-30 12:02:39 +0500

    created interfaces for services

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Notifications/Services/ISmsTemplateService.cs
@@ -0,0 +1,45 @@
+﻿using TruckWorld.Application.Common.Models.Querying;
+using TruckWorld.Domain.Entities;
+using TruckWorld.Domain.Enums;
+
+namespace TruckWorld.Application.Common.Notifications.Services
+{
+    public interface ISmsTemplateService
+    {
+        /// <summary>
+        /// it takes smsTemplate by filtering
+        /// </summary>
+        /// <param name="pagination"></param>
+        /// <param name="asNoTracking"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Ilist SmsTemplate</returns>
+        ValueTask<IList<SmsTemplate>> GetByFilterAsync(
+            FilterPagination pagination,
+            bool asNoTracking = false,
+            CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// it takes smsTemplate By type
+        /// </summary>
+        /// <param name="templateType"></param>
+        /// <param name="asNoTracking"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>default or smsTemplate</returns>
+        ValueTask<SmsTemplate?> GetByTypeAsync(
+            NotificationTemplateType templateType,
+            bool asNoTracking = false,
+            CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// it creates by emailTemplate
+        /// </summary>
+        /// <param name="smsTemplate"></param>
+        /// <param name="saveChanges"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>smsTemplate</returns>
+        ValueTask<SmsTemplate> CreateAsync(
+            SmsTemplate smsTemplate,
+            bool saveChanges = true,
+            CancellationToken cancellationToken = default);
+    }
+}
