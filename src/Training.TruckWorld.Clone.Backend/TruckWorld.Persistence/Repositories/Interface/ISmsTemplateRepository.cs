f699eb9584005b3c1ae2539ddea8c30b975e5ded
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-11-30 12:16:09 +0500

    created interfaces and their services for repositories

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Persistence/Repositories/Interface/ISmsTemplateRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+using TruckWorld.Domain.Entities;
+
+namespace TruckWorld.Persistence.Repositories.Interface;
+
+/// <summary>
+/// Repository interface for managing smsTemplate-related data operations.
+/// </summary>
+public interface ISmsTemplateRepository
+{
+    /// <summary>
+    /// Retrieve a collection of smsTemplate based on the specified predicate. 
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <param name="asNoTracking"></param>
+    /// <returns>An IQueryable collection of SmsTemplate objects</returns>
+    IQueryable<SmsTemplate> Get(
+        Expression<Func<SmsTemplate,
+        bool>>? predicate = default,
+        bool asNoTracking = false);
+
+    /// <summary>
+    /// Create a collection of smsTemplate based on the specified predicate. 
+    /// </summary>
+    /// <param name="smsTemplate"></param>
+    /// <param name="saveChanges"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>An IQueryable collection of SmsTemplate objects</returns>
+    ValueTask<SmsTemplate> CreateAsync(
+        SmsTemplate smsTemplate,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default);
+}
