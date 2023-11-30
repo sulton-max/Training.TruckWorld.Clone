faff4c5e8c47138ca1751f5932aba0d2058f720d
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-11-30 12:18:02 +0500

    created NotificationsController

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Api/Controllers/NotificationsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TruckWorld.Application.Common.Models.Querying;
+using TruckWorld.Application.Common.Notifications.Services;
+
+namespace TruckWorld.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NotificationsController : ControllerBase
+{
+    private readonly IEmailTemplateService _emailTemplateService;
+    private readonly ISmsTemplateService _smsTemplateService;
+
+    public NotificationsController(IEmailTemplateService emailTemplateService, ISmsTemplateService smsTemplateService)
+    {
+        _emailTemplateService = emailTemplateService;
+        _smsTemplateService = smsTemplateService;
+    }
+
+    [HttpGet("getSmsTemplates")]
+    public async ValueTask<IActionResult> GetSmsTemplates([FromQuery] FilterPagination pagination)
+    {
+        var result = await _smsTemplateService.GetByFilterAsync(pagination);
+        return Ok(result);
+    }
+
+    [HttpGet("getEmailTemplates")]
+    public async ValueTask<IActionResult> GetEmailTemplates([FromQuery] FilterPagination filterPagination)
+    {
+        var result = await _emailTemplateService.GetByFilterAsync(filterPagination);
+        return Ok(result);
+    }
+}
