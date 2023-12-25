dabf9fbaef8ee1e5062ac626f5be6c154fbe807d
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2023-12-25 20:26:32 +0500

    implemented notification rendering services

---
+++ src/Training.TruckWorld.Clone.Backend/TruckWorld.Application/Common/Notifications/Services/ISmsRenderingService.cs
@@ -0,0 +1,21 @@
+﻿using TruckWorld.Application.Common.Notifications.Models;
+
+namespace TruckWorld.Application.Common.Notifications.Services;
+
+/// <summary>
+/// Represents a service for rendering sms message
+/// </summary>
+public interface ISmsRenderingService
+{
+
+    /// <summary>
+    /// Asynchronously renders sms messages
+    /// </summary>
+    /// <param name="smsMessage"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<string> RenderAsync(
+        SmsMessage smsMessage,
+        CancellationToken cancellationToken = default
+    );
+}
